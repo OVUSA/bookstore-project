8dc10d20c894fc1d6bd7b6c8bcd64b1218148ddc
tekenthusiast <vamshigokulreddy@gmail.com>
2021-11-07 21:19:13 -0600

    VisualStudio

--- BookStore/src/com/ebook/model/service/Order/OrderService.java
+++ BookStore/src/com/ebook/model/service/Order/OrderService.java
@@ -13,8 +13,6 @@ public interface PartnerService {
 	public Set<PartnerRepresentation> getPartners();
 	public PartnerRepresentation getPartner(int Id);
 //	public Partner_Representation createPartner(Partner partner); FIX ME
-
-   
-    
+	System.out.println("000---------------------Orderservice---------------")
 
 }

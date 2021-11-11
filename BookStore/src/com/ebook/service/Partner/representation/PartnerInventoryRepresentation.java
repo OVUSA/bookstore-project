37dfe27669006e86d89329ade5e292adf23bc267
ovusa <velichko.olga.chicago@gmail.com>
2021-11-11 16:31:49 -0600

    added PartnerInventory service layer

---
+++ BookStore/src/com/ebook/service/Partner/representation/PartnerInventoryRepresentation.java
@@ -0,0 +1,46 @@
+package com.ebook.service.Partner.representation;
+
+
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "PartnerInventory")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "")
+
+public class PartnerInventoryRepresentation {
+	private String partnerId;
+	private String productId;
+	private String productName;
+	private double price;
+	
+	
+
+	public String getPartnerId() {
+		return partnerId;
+	}
+	public void setPartnerId(String partnerId) {
+		this.partnerId = partnerId;
+	}
+	public String getProductId() {
+		return productId;
+	}
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+}
---
+++ BookStore/src/com/ebook/service/Partner/representation/PartnerInventoryRequest.java
@@ -0,0 +1,36 @@
+package com.ebook.service.Partner.representation;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "")
+
+public class  PartnerInventoryRequest {
+	private String partnerId;
+	private String productTitle;
+	private double productPrice;
+
+	public String getPartnerId() {
+		return partnerId;
+	}
+	public void setPartnerId(String partnerId) {
+		this.partnerId = partnerId;
+	}
+	public String getProductTitle() {
+		return productTitle;
+	}
+	public void setProductTitle(String productTitle) {
+		this.productTitle = productTitle;
+	}
+	public double getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(double productPrice) {
+		this.productPrice = productPrice;
+	}
+	
+}

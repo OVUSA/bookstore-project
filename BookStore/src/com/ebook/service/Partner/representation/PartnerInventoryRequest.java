884f4c3bc10d9fca9c7f2396549dfc0d604cebe5
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-12-04 23:06:43 -0600

    changed representation

--- BookStore/src/com/ebook/service/Partner/representation/PartnerInventoryRequest.java
+++ BookStore/src/com/ebook/service/Partner/representation/PartnerInventoryRequest.java
@@ -4,74 +4,37 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
+import com.ebook.service.AbstractRepresentation.AbstractRepresentation;
+import com.ebook.service.item.representation.ProductRepresentation;
 
-import com.ebook.service.item.representation.ProductRequest;
-
-@XmlRootElement
+@XmlRootElement(name = "PartnerInventory")
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "")
 
-public class  PartnerInventoryRequest {
-	
-	private String title;
-	private double price;
-	private String description;
-	private String author;
+public class PartnerInventoryRepresentation extends AbstractRepresentation  {
+	private ProductRepresentation product;
 	private int quantity;
 	
-	PartnerInventoryRequest(){
+	public PartnerInventoryRepresentation(){
 		
 	}
-	PartnerInventoryRequest(String title, String description,String author,double price, int quantity){
-		this.author = author;
-		this.title = title;
-		this.description = description;
-		this.price = price;
-		this.quantity= quantity;			
-		}
-	PartnerInventoryRequest(String title){
-		this.title = title;
-		
-	}
-
 	
-	public String getTitle() {
-		return title;
+	public PartnerInventoryRepresentation(ProductRepresentation product, int amount){
+		this.product = product;
+		this.quantity = amount;
 	}
 	
-	public void setTitle(String title) {
-		this.title = title;
+	public ProductRepresentation getProduct() {
+		return product;
 	}
-	
-	public double getPrice() {
-		return price;
+	public void setProduct(ProductRepresentation product) {
+		this.product = product;
 	}
-	
-	public void setPrice(double price) {
-		this.price = price;
-	}
-	
-	public String getDescription() {
-		return description;
-	}
-	
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	
-	public String getAuthor() {
-		return author;
-	}
-	
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
 	public int getQuantity() {
 		return quantity;
 	}
-
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
+
 }
